d53b88534f83e66e4dbf9bff2662e9eae43707c2
andrymahdison <andrymahdison@gmail.com>
2018-08-05 20:34:29 +0700

    refactor index method framework

--- method/user/index.js
+++ method/user/index.js
@@ -23,49 +23,24 @@ for(let i=0; i < files.length; i++) {
     const method = require(path.join(__dirname, files[i]));
     const MIDDLEWARE = method.MIDDLEWARE || function(req, res, next) { next(); };   
 
+    const func = Promise.coroutine(
+        function* (request, response) {
+        const result = yield method.MAINFUNCTION(request, ResponseHandler);
+        response.status(result.status).json(result.data);
+    });
+
     switch (method.METHODTYPE) {
         case 'GET':
-            router.get(
-                method.ENDPOINT, 
-                MIDDLEWARE, 
-                Promise.coroutine(
-                    function* (request, response) {
-                        const result = yield method.MAINFUNCTION(request, ResponseHandler);
-                        response.status(result.status).json(result.data);
-            }));
+            router.get(method.ENDPOINT, MIDDLEWARE, func);
             break;
         case 'POST':
-            router.post(
-                method.ENDPOINT, 
-                MIDDLEWARE, 
-                Promise.coroutine(
-                    function* (request, response) {
-                        const result = yield method.MAINFUNCTION(request, ResponseHandler);
-                        response.status(result.status).json(result.data);
-                })
-            );
+            router.post(method.ENDPOINT, MIDDLEWARE, func);
             break;
         case 'PUT':
-            router.put(
-                method.ENDPOINT, 
-                MIDDLEWARE, 
-                Promise.coroutine(
-                    function* (request, response) {
-                    const result = yield method.MAINFUNCTION(request, ResponseHandler);
-                    response.status(result.status).json(result.data);
-                })
-            );
+            router.put(method.ENDPOINT, MIDDLEWARE, func);
             break;
         case 'DELETE':
-            router.delete(
-                method.ENDPOINT, 
-                MIDDLEWARE, 
-                Promise.coroutine(
-                    function* (request, response) {
-                    const result = yield method.MAINFUNCTION(request, ResponseHandler);
-                    response.status(result.status).json(result.data);
-                })
-            );
+            router.delete(method.ENDPOINT, MIDDLEWARE, func);
             break;
         default:
             break;
